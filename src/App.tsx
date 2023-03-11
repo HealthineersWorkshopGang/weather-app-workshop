dead1428e4fe0702026daf08f2834c8a72b53a08
HealthineersGang <HealthineersWorkshopGang@gmail.com>
2023-03-11 14:08:17 +0100

    PWM: AdditionalData accepts WeatherDataType

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./App.scss";
 import { AdditionalData } from "./components/AdditionalData/";
-import { WidgetProps } from "./components/AdditionalData/Widget";
 import { Controls } from "./components/Controls";
 import { Header } from "./components/Header";
 import { WeatherData } from "./components/WeatherData";
@@ -10,30 +9,12 @@ import { WeatherDataType } from "./types";
 function App() {
   const [weatherData, setWeatherData] = useState<WeatherDataType | null>(null);
 
-  const data: WidgetProps[] = [
-    {
-      value: weatherData?.hummidity || 50,
-      unit: "%",
-      text: "Humidity",
-    },
-    {
-      value: weatherData?.wind || 21,
-      unit: "km/h",
-      text: "Wind Speed",
-    },
-    {
-      value: weatherData?.pressure || 1010,
-      unit: "kPa",
-      text: "Pressure",
-    },
-  ];
-
   return (
     <div className="App">
       <Header title={weatherData?.name || "City was not set"} />
       <Controls setWeatherData={setWeatherData} />
       <WeatherData />
-      <AdditionalData data={data} />
+      <AdditionalData weatherData={weatherData} />
     </div>
   );
 }
--- src/components/AdditionalData/AdditionalData.tsx
+++ src/components/AdditionalData/AdditionalData.tsx
@@ -1,16 +1,38 @@
-import Widget, { WidgetProps } from "./Widget";
+import { WeatherDataType } from "../../types";
 import "./additionaldata.scss";
+import Widget, { WidgetProps } from "./Widget";
+
+const createData = (weatherData: WeatherDataType | null) => {
+  return [
+    {
+      value: weatherData?.hummidity || 50,
+      unit: "%",
+      text: "Humidity",
+    },
+    {
+      value: weatherData?.wind || 21,
+      unit: "km/h",
+      text: "Wind Speed",
+    },
+    {
+      value: weatherData?.pressure || 1010,
+      unit: "kPa",
+      text: "Pressure",
+    },
+  ] as WidgetProps[] ;
+}
 
 
 
 type AdditionalDataProps = {
-  data: WidgetProps[]
+  weatherData: WeatherDataType | null
 }
 
-function AdditionalData({ data }: AdditionalDataProps) {
+function AdditionalData({ weatherData }: AdditionalDataProps) {
+
   return (
     <div className="additional-data">
-      {data.map((props) => (
+      {createData(weatherData).map((props) => (
         <Widget
           key={props.text}
           value={props.value}
