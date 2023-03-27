3abb98d8b3c71b115163153831c881cf136cdba3
HealthineersGang <HealthineersWorkshopGang@gmail.com>
2023-03-27 19:52:01 +0200

    PWM: LocationList improvements

--- src/components/LocationList/LocationList.tsx
+++ src/components/LocationList/LocationList.tsx
@@ -1,35 +1,35 @@
 import { GeoData, Location } from "../../types";
+import "./LocationList.scss";
 
 type LocationListProps = {
-    locations: Location[];
-    setGeoData: React.Dispatch<React.SetStateAction<GeoData>>
-}
+  locations: Location[];
+  setGeoData: React.Dispatch<React.SetStateAction<GeoData>>;
+};
 
 function prettyLocationName(location: Location) {
-    return `${location.name} | ${location.state} - ${location.country}`;
+  return `${location.name} | ${location.state} - ${location.country}`;
 }
 
 export const LocationList = ({ locations, setGeoData }: LocationListProps) => {
+  function onHandleLocationSelect(location: Location) {
+    console.log(location.lat, location.lon);
+    setGeoData({
+      lat: location.lat,
+      lon: location.lon,
+    });
+  }
 
-    function onHandleLocationSelect(location: Location) {
-        console.log(location.lat, location.lon);
-        setGeoData({
-            lat: location.lat,
-            lon: location.lon
-        });
-    }
-
-    return (
-        <ul>
-            {
-                locations.map((location: Location, index) => (
-                    <li key={index} onClick={() => onHandleLocationSelect(location)}>
-                        {
-                            prettyLocationName(location)
-                        }
-                    </li>
-                ))
-            }
-        </ul>
-    )
-}
+  return (
+    <div className="location-list">
+      <ul>
+        {locations.map((location: Location, index) => (
+          <li key={index}>
+            <button onClick={() => onHandleLocationSelect(location)}>
+              {prettyLocationName(location)}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
